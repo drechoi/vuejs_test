e3ecb210eea22ac44741a998607c0b537ea58e5f
andre <andre.cfh@gmail.com>
2019-05-30 17:16:11 +0800

    saparated menu into a component

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,15 @@
 <template>
   <div id="app">
-    <b-navbar toggleable="md" type="dark" variant="dark">
-      <b-navbar-brand to="/">
-      	<b-img src="static/favicon.png" height="30" alt="icon"></b-img>
-      	{{appName}}
-      </b-navbar-brand>
-      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-
-      <b-collapse is-nav id="nav_collapse">
-        <b-navbar-nav class="ml-auto">
-          <!-- menu items with authentication -->
-          <template v-if="isAuthenticated">
-            <b-nav-item to="/showData" active-class="active" exact>Data</b-nav-item>
-            <b-nav-item to="/wishList" active-class="active" exact>Wish List</b-nav-item>
-          </template>          
-
-          <!-- profile item -->
-          <template v-if="!isAuthenticated">
-            <b-nav-item @click.prevent="login" active-class="active" exact right>Login</b-nav-item>
-          </template>
-          <template v-else>
-            <b-nav-item-dropdown right>
-              <!-- Using 'button-content' slot -->
-              <template slot="button-content"><em>{{ profile.name }}</em></template>
-              <b-dropdown-item to="/profile">Profile</b-dropdown-item>
-              <b-dropdown-item @click.prevent="logout">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
-          </template>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-    <router-view/>
+    <topNav v-bind:appName="appName" />
+    <b-container>
+      <router-view/>
+    </b-container>
   </div>
 </template>
 
 <script>
+import topNav from '@/components/TopMenu';
+
 export default {
   name: 'App',
   data() {
@@ -43,6 +18,9 @@ export default {
       isAuthenticated: false,
       profile: this.$auth.profile
     };
+  },
+  components: {
+    topNav,
   },
   async created() {
     try {
