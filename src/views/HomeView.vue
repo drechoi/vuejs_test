fb37d8631bfca9416e5e0766c2d75a76abae5fe4
andre <andre.cfh@gmail.com>
2019-06-03 09:58:00 +0800

    rename file for case sensitivity

---
+++ src/views/HomeView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <p>
+      --- this is Debug ---
+    </p>
+    <p>
+      --- this is Current user info ---
+    </p>
+    <UserInfo user-name="Bartholomew" />
+    <p />
+
+    <UserInfo user-name="Caitlyn"/>
+
+    <p>
+      --- this is wish list ---
+    </p>
+    <WishList :todos="$store.state.wish.wishItems" />
+  </div>
+</template>
+
+<script>
+import WishList from '@/components/wishList';
+import WishItem from '@/components/wishItem';
+import UserInfo from '@/components/UserInfo';
+
+export default {
+  components: {
+    WishList,
+    WishItem,
+    UserInfo,
+  },
+  computed: {
+  },
+  methods: {
+    handleOk(bvModalEvt) {
+      // Prevent modal from closing
+      alert('handle ok!' + this.name);
+    },
+    handleSubmit() {
+      alert('handle submit!');
+    }
+  }
+};
+</script>
